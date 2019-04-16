1e2ffa8b129451d952fd6c1122a799861e608ac4
Nolan Wagner <NolanDubyu@gmail.com>
2019-04-16 13:22:52 -0400

    index

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -17,18 +17,6 @@
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>
 <body>
-    <header>
-        <nav>
-            <strong>{{ $page->site->title }}</strong><br>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/posts">Posts</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-
     <article>
         <section>
             @yield('content')
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,40 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
-
-    <h2>Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+<div class="main">
+	<div class="leaf-left"><?xml version="1.0" encoding="utf-8"?>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 246 584.6" style="enable-background:new 0 0 246 584.6;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#76E870;}
+</style>
+<path class="st0" d="M7.4,243.8c-0.7-35.8-1.5-48.7,1.5-67.1s20.3-21,26.9-19.5c6.6,1.5,39.8,30.2,18.4-14.8s4.1-87,18.8-92.9
+	s18.4,17,25.5,37.3s9.2,24.7,32.1-12.9s93.3-92.9,109.9-67.1s-8.1,58.3-23.6,85.9s-10,37.6,0.7,53.9c10.7,16.2,11.8,30.6-24.3,59.4
+	s-15.9,46.8-15.9,46.8s18.8,13.3,3.7,31.4c-15.1,18.1-91.5,75.6-103.3,80.4c-11.8,4.8-12.2-2.2-12.5-7.7c-0.4-5.5,0-17.3,8.5-49.8
+	s73-193.3,73-193.3S42.4,220.2,0,584.6C0,584.6,8.1,279.6,7.4,243.8z"/>
+</svg>
+</div>
+	<div class="content-section">
+		<div class="header">
+	<a href=""><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/529582/Celery%20City%20Media%20Logo.png" class="logo" /></a>
+	<div class="nav">
+		<a href="/"><div class="nav-item">What We Do</div></a>
+		<a href="/"><div class="nav-item">Our Work</div></a>
+			<a href="/"><div class="nav-item">Contact</div></a>
+	</div>
+		</div>
+		<div class="callout-container">
+	<div class="callout-section">
+		<h1 class="main-callout">We help small businesses improve their<br/> local search ranking with Google.</h1>
+		<div class="btn-section">
+			<a href=""><div class="btn btn-black">Audit Your Website</div></a>
+		<a href=""><div class="btn btn-green">Free Quote</div>
+			</div></a>
+	</div>
+		</div>
+	</div>
+	<div class="leaf-right">
+		<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246.02 584.43"><defs><style>.cls-1{fill:#76e870;}</style></defs><title>Celery-Leaf-2</title><path class="cls-1" d="M246,584.6c-42.4-364.4-146.8-470.8-146.8-470.8s64.5,160.8,73,193.3,8.9,44.3,8.5,49.8-.7,12.5-12.5,7.7S80,302.3,64.92,284.2s3.7-31.4,3.7-31.4,20.2-18-15.9-46.8-35-43.2-24.3-59.4,16.2-26.3.7-53.9S-11.08,32.6,5.52,6.8s87,29.5,109.9,67.1,25,33.2,32.1,12.9,10.8-43.2,25.5-37.3,40.2,47.9,18.8,92.9,11.8,16.3,18.4,14.8,23.9,1.1,26.9,19.5,2.2,31.3,1.5,67.1S246,584.6,246,584.6Z" transform="translate(0 -0.17)"/></svg>
+	</div>
+</div>
 @endsection
