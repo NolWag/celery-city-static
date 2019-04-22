1d175d541886ce9072b8d1ba380c69bac5b030eb
Nolan Wagner <NolanDubyu@gmail.com>
2019-04-22 17:20:30 -0400

    blog

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -3,11 +3,9 @@
 @section('title', 'Posts')
 
 @section('content')
-    <h1>Posts</h1>
-
-    <ul>
+    <ul class="post-list">
         @forelse ($posts->sortBy('title') as $post)
-            <li>
+            <li class="post-item">
                 <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
                 <small>{{ date('M j, Y', $post->date) }}</small>
             </li>
