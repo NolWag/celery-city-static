e1d617997e9cc593bf4ff26fc889bb16ba38ec8d
Nolan Wagner <NolanDubyu@gmail.com>
2019-05-13 13:08:06 -0400

    new pages

---
+++ source/google-business-listing.blade.php
@@ -0,0 +1,32 @@
+@extends('_layouts.master')
+
+@section('title', 'Google My Business Listing')
+
+@section('content')
+<div class="container">
+    <div class="section">
+    <h2>What is it?</h2>
+    <p>Consider Google Business Listing the very first step in your web presence /  digital marketing campaign. Without this listing it makes it difficult for Google to categorize your business on their own and as a result, it becomes more difficult for your customers to find you.</p>
+    <p>The listing is a formalized version of your business information: Address, phone number, hours, etc. When performing a search for local services, it is often referred to as the “3 pack”, which refers to the 3 local business listings at the top of the search results.</p>
+    <p>Once confirmed, the business will start to appear in the local listings and you will have access to the dashboard and the many tools offered by Google to improve your local search ranking.</p>
+    
+    <h2>Why should I claim it?</h2>
+    <p>By claiming your business listing and managing it on your own, your formalizing the information about your business, versus letting Google guess about this information.</p>
+    <p>88% of consumers consult reviews before making a purchase.</p>
+    <p>97% of consumers search for local products and services with a search engine, but only 37% of businesses have claimed a Google My Business local business listing.</p>
+    
+    <h2>What do I need to do?</h2>
+    <p>This service can be performed for businesses that don’t have a website yet. We will send over a form to be completed with your businesses basic information.</p>
+    <p>Once we have submitted the claim to Google it normally takes about a week to receive a confirmation code in the mail. At this point you will have full access to the listing dashboard where we can start adding the correct info about your business and reply to unanswered questions and reviews.</p>
+    <div class="btn-section flex-start">
+    <a href="/contact"><div class="btn btn-black">Services</div></a>
+    <a href="/contact"><div class="btn btn-black">Contact Us</div></a>
+    </div>
+
+    
+ 
+    
+    </div>
+    
+</div>
+@endsection
